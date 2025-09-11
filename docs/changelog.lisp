cf4c5fd04e10883c1e6fb081957a33b585f18f5c
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-09-11 19:08:28 +0000

    Correct date.

--- docs/changelog.lisp
+++ docs/changelog.lisp
@@ -12,5 +12,5 @@
                :external-docs ("https://40ants.com/40ants-pg/"))
   (0.2.0 2025-09-11
          "* Removed 40ants-bots/db package. Now for database connection, 40ANTS-PG/CONNECTION:WITH-CONNECTION macro should be used.")
-  (0.1.0 2023-02-05
+  (0.1.0 2025-07-26
          "* Initial version."))
