24bacb852501557d83569c9c4e376d2b3dde46d4
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-12-11 21:15:34 +0000

    Added a new function send-chat-action.

--- src/api.lisp
+++ src/api.lisp
@@ -14,8 +14,11 @@
                 #:chat-platform-id)
   (:import-from #:log4cl-extras/error
                 #:with-log-unhandled)
+  (:import-from #:str
+                #:replace-all)
   (:export
-   #:send-message))
+   #:send-message
+   #:send-chat-action))
 (in-package #:40ants-bots/api)
 
 
@@ -53,3 +56,40 @@
                message)
       (push message *collected-messages*))
     (values message)))
+
+
+
+(defun-with-same-keys (send-chat-action cl-telegram-bot2/api:send-chat-action)
+                      (chat-or-user action &rest rest)
+  "Use this method when you need to tell the user that something is happening on the bot's side.
+
+   Second argument should be a keyword denoting an action to broadcast.
+   Choose one, depending on what the user is about to receive:
+
+   - :TYPING for text messages
+   - :UPLOAD-PHOTO for photos
+   - :RECORD-VIDEO or :UPLOAD-VIDEO for videos
+   - :RECORD-VOICE or :UPLOAD-VOICE for voice notes
+   - :UPLOAD-DOCUMENT for general files
+   - :CHOOSE-STICKER for stickers
+   - :FIND-LOCATION for location data
+   - :RECORD-VIDEO_note or :UPLOAD-VIDEO_note for video notes"
+  (let* ((chat-id (%get-chat-id-from chat-or-user)))
+    (handler-case
+        (with-log-unhandled ()
+          (let ((action (replace-all "-" "_"
+                                     (string-downcase action))))
+            (apply #'cl-telegram-bot2/api:send-chat-action
+                   chat-id
+                   action
+                   rest)))
+      (telegram-error (err)
+        ;; Process
+        ;; Forbidden: bot was blocked by the user
+                      
+        (when (string= (error-description err)
+                       "Bad Request: need administrator rights in the channel chat")
+          (log:warn "Unable to reply to chat ~S because bot needs administration rights on this channel"
+                    chat-id)
+          (return-from send-chat-action nil))))
+    (values)))
